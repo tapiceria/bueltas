fcb35eaadaf5e18e6fe6cebaffd5e7c121406e1f
tapiceria <tapiceria.ramos.col@gmail.com>
2020-10-30 19:57:30 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a,b ,resultado=0;
+cout<<"ingrese valor del producto :";cin>>a;
+cout<<"ingrese Valor de Billete con el que pagan:  ";cin>>b;
+resultado = (b-a);
+cout<<"total de Vueltas a DAR : "<<resultado;
+
+
+
+return 0;
+}
